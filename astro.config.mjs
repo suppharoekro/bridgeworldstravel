51858f2e9d1101e5d5c56e391a6ec7edd601ad79
suppharoekro <suppharoek.346@gmail.com>
2026-04-17 13:56:04 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://bridgeworldstravel.com',
+  site: "https://bridgeworldstravel.com",
+  integrations: [mdx()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
